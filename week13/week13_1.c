2b001190135998291a63c21ecbdfeac5c908be69
mervecplt <canpolatmerveb432a3@gmail.com>
2025-03-05 20:27:36 +0300

    c

--- week13/week13_1.c
+++ week13/week13_1.c
@@ -1,6 +1,6 @@
 // Task 1
 #include <stdio.h>
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
     printf("Number of arguments including file name: %d\n", argc);
     for (int i = 1; i < argc; i++) {
         printf("Argument %d: %s\n", i, argv[i]);
